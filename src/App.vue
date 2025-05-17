52e76fc28caa61301d60b9567d49f11f22a04515
Bernardo Rocha <programadorbernardords@gmail.com>
2025-05-17 04:22:19 -0400

    Primeiro Commit incompleto

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="column is-half is-offset-one-quarter">
+        <img src="./assets/logo.png" alt="">
+        <hr>
+        <h4 class="is-size-1">Pokedexx</h4>
+      <div v-for="(poke, index) in pokemons" :key="index">
+        <PokemonInfo :name="poke.name" :num="poke.num" :url="poke.url" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import PokemonInfo from "./components/PokemonInfo.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      pokemons: [],
+    };
+  },
+  created: function () {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
+      .then((res) => {
+        console.log("Pegou lista de Requisição básica!");
+        this.pokemons = res.data.results;
+      });
+  },
   components: {
-    HelloWorld
-  }
-}
+    PokemonInfo,
+  },
+};
 </script>
 
 <style>
+@import "https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
